a11be20ebee9b9ed846f0adf70bace8e76a0c0d0
Christian Choi <krelle@gmail.com>
2015-01-25 00:52:37 +0100

    A simple usage example

---
+++ examples/example.php
@@ -0,0 +1,24 @@
+<?php
+
+// Load composer dependencies
+require_once realpath(__DIR__.'/../vendor/autoload.php');
+
+// Instantiate Config-object
+$config = new Choi\TheTvDbApi\Config(require realpath(__DIR__.'/config.php'));
+
+// Instantiate Manager-object
+$thetvdb = new Choi\TheTvDbApi\Manager($config);
+
+// Search series by name
+$search_results = $thetvdb->searchSeries('Arrow');
+
+// Retrieve Series-object
+$series = $thetvdb->getSeriesById($search_results->first()['seriesid']);
+
+// Fetch base series details
+$base_details = $series->getBaseDetails();
+
+// Fetch full series details
+$full_details = $series->getFullDetails();
+
+dd($full_details);
